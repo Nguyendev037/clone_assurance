7aede744fbc16415f6118ca8666354367c030cc4
Nguyendev037 <nguyenndev037@gmail.com>
2024-11-18 15:58:17 +0700

    set up inital backend

---
+++ Backend/src/test/java/com/example/Durian_Assurance/LearnAgainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Durian_Assurance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAgainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
