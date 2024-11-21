3f3125cbbe72cf8d994cb131c25b0ddeff78c4b5
Nguyendev037 <nguyenndev037@gmail.com>
2024-11-21 14:02:13 +0700

    add base axios

--- Frontend_Admin/src/components/Table.tsx
+++ Frontend_Admin/src/components/Table.tsx
@@ -1,8 +1,25 @@
-﻿import React from 'react'
+﻿import React, {useEffect, useState} from 'react'
+import {Base_Axios} from "../axios.ts";
+interface PropPass {
+    model: String;
+}
 
+export default function LandingPage({model}: PropPass) {
 
+    const [data,Setdata] = useState({});
+    const fetchAllData = async () => {
+        try {
+            const response = await Base_Axios.get(`/${model}`);
+            return response.data;
+        } catch (error) {
+            console.error(error);
+        }
+    }
 
-export default function Table() {
+    useEffect(()=> {
+        Setdata(fetchAllData());
+    },[])
+
     return (
         <section className="table-auto">
         </section>
