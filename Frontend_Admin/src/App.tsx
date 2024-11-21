24fcbe41ee3e95988b973131443fd6aa4bf2ac74
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2024-11-21 15:06:45 +0700

    add tailwind css and navbar

--- Frontend_Admin/src/App.tsx
+++ Frontend_Admin/src/App.tsx
@@ -2,13 +2,39 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SignIn from "./pages/SignIn";
+import Navbar from "./components/Navbar";
+
+const routes = [
+  {
+    path: "/",
+    component: LandingPage,
+    title : "Home"
+  },
+  {
+    path: "/sign-in",
+    component: SignIn,
+    title : "Sign in"
+  },
+];
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LandingPage />}/>
-        <Route path="sign-in" element={<SignIn />} />
+        {routes.map((route, index) => {
+          return (
+            <Route
+              key={index}
+              path={route.path}
+              element={
+                <>
+                  <Navbar routes={routes}/>
+                  {<route.component />}
+                </>
+              }
+            ></Route>
+          );
+        })}
       </Routes>
     </Router>
   );
