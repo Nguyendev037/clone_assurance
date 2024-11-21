257d761b9112e055053c16e6ecd87b8e7cfbb33d
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2024-11-21 14:43:31 +0700

    sign in page

--- Frontend_Admin/src/App.tsx
+++ Frontend_Admin/src/App.tsx
@@ -1,11 +1,17 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import SignIn from "./pages/SignIn";
 
 function App() {
   return (
-      <>
-    
-      </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />}/>
+        <Route path="sign-in" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ Frontend_Admin/src/components/Snackbar.tsx
@@ -0,0 +1,25 @@
+import React, { useEffect } from "react";
+
+function Snackbar({
+  open,
+  setOpen,
+  text,
+}: {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  text: string;
+}) {
+  useEffect(() => {
+    setTimeout(() => {
+      setOpen(false);
+    }, 3000);
+  }, [open]);
+
+  return (
+    <div id="snackbar" className={open ? "show" : ""}>
+      {text}
+    </div>
+  );
+}
+
+export default Snackbar;
