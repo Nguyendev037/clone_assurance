05ed0548a646b79b8a920a69a42974f8c06188fc
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2024-11-22 15:15:42 +0700

    add claim form

--- Frontend_Admin/src/pages/UserClaims.tsx
+++ Frontend_Admin/src/pages/UserClaims.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Modal from '../components/Modal';
+import CreateClaimForm from '../components/CreateClaimForm';
 
 function UserClaims() {
+    const [open,setOpen] = useState<boolean>(false);
+    
+
+    function createClaim(id : number){
+        
+    }
+
   return (
     <div>
-      
+      <Modal buttonText="accept claim" open={open} setOpen={setOpen} content={<CreateClaimForm />} />
     </div>
   )
 }
