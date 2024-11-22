4596628684e0b1b011a8f4bb1caeed79b77b23ff
Nguyendev037 <nguyenndev037@gmail.com>
2024-11-22 15:14:46 +0700

    add claim table

--- Frontend_Admin/src/pages/UserClaims.tsx
+++ Frontend_Admin/src/pages/UserClaims.tsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import Table from "../components/Table.tsx";
+import {Base_Axios} from "../axios.ts";
 
 function UserClaims() {
+  const [data, setData] = useState([]); // Use camelCase for setter function
+  const model = "claim";
+  // Fetch Data
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await Base_Axios.get(`/${model}`);
+        console.log("API Response:", response.data);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, [model]);
   return (
     <div>
-      
+      <>
+        <Table
+            model={model}
+            data={data}
+            buttons={[
+          
+            ]}
+        />
+      </>
     </div>
   )
 }
